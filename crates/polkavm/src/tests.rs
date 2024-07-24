9a2d57217713733cac33d55638e02d4477b7e780
Alan Bertani <alanbertani051@gmail.com>
2024-07-24 15:20:12 -0300

    tests: deleted functions and inlined the code in the test

--- crates/polkavm/src/tests.rs
+++ crates/polkavm/src/tests.rs
@@ -1,6 +1,7 @@
 use crate::mutex::Mutex;
 use crate::{
-    config, CallArgs, Caller, CallerRef, Config, Engine, ExecutionError, Gas, GasMeteringKind, Linker, MemoryMap, Module, ModuleConfig, ProgramBlob, Reg, StateArgs, Trap
+    CallArgs, Caller, CallerRef, Config, Engine, ExecutionError, Gas, GasMeteringKind, Linker, MemoryMap, Module, ModuleConfig,
+    ProgramBlob, Reg, StateArgs, Trap,
 };
 use alloc::collections::BTreeMap;
 use alloc::rc::Rc;
@@ -516,49 +517,7 @@ impl TestInstance {
     }
 }
 
-fn blob_null_pointer_djump() -> ProgramBlob {
-    let mut builder = ProgramBlobBuilder::new();
-    builder.set_rw_data_size(0x4000);
-    builder.add_export_by_basic_block(0, b"main");
-    builder.set_code(
-        &[
-            asm::jump_indirect(Reg::A0, 0x0),
-        ],
-        &[0x0],
-    );
-    ProgramBlob::parse(builder.into_vec().into()).unwrap()
-}
 
-fn blob_miss_aligned_pointer_djump() -> ProgramBlob {
-    let mut builder = ProgramBlobBuilder::new();
-    builder.set_rw_data_size(0x4000);
-    builder.add_export_by_basic_block(0, b"main");
-    builder.set_code(
-        &[
-            asm::load_imm(Reg::A0, 0x1000),
-            asm::jump_indirect(Reg::A0, 1)
-        ],
-        &[1],
-    );
-    
-    ProgramBlob::parse(builder.into_vec().into()).unwrap()
-}
-
-fn setup_instance(blob: ProgramBlob) -> crate::Instance<()>{
-    let _ = env_logger::try_init();
-
-    let mut config = crate::Config::default();
-    config.set_backend(Some(crate::BackendKind::Compiler));
-    config.set_sandbox(Some(crate::SandboxKind::Linux));
-
-    let engine = Engine::new(&config).unwrap();
-    let linker: Linker<()> = Linker::new(&engine);
-
-    let module = Module::from_blob(&engine, &Default::default(), blob).unwrap();
-    let instance_pre = linker.instantiate_pre(&module).unwrap();
-
-    instance_pre.instantiate().unwrap()
-}
 
 fn test_invalid_jump_trap(config: Config) {
     // TODO: This test is currently failing as expected. Once the implementation is complete, remove this early return
@@ -566,16 +525,30 @@ fn test_invalid_jump_trap(config: Config) {
 
     let _ = config;    
     let state_args1 = StateArgs::default();
-    let state_args2 = StateArgs::default();
 
-    let null_pointer_instance = setup_instance(blob_null_pointer_djump());
-    let miss_aligned_pointer_instance = setup_instance(blob_miss_aligned_pointer_djump());
+    let mut builder = ProgramBlobBuilder::new();
+    builder.add_export_by_basic_block(0, b"main");
+    builder.set_code(
+        &[
+            asm::jump_indirect(Reg::A0, 0x0),
+        ],
+        &[],
+    );
     
+    let mut config = crate::Config::default();
+    config.set_backend(Some(crate::BackendKind::Compiler));
+    config.set_sandbox(Some(crate::SandboxKind::Linux));
+
+    let engine = Engine::new(&config).unwrap();
+    let linker: Linker<()> = Linker::new(&engine);
+
+    let module = Module::from_blob(&engine, &Default::default(), ProgramBlob::parse(builder.into_vec().into()).unwrap()).unwrap();
+    let instance_pre = linker.instantiate_pre(&module).unwrap();
+
+
+    let null_pointer_instance = instance_pre.instantiate().unwrap();
     let ext_main1 = null_pointer_instance.module().lookup_export("main").unwrap();
     let result1 = null_pointer_instance.call(state_args1, CallArgs::new(&mut (), ext_main1));
-    
-    let ext_main2 = miss_aligned_pointer_instance.module().lookup_export("main").unwrap();
-    let result2 = miss_aligned_pointer_instance.call(state_args2, CallArgs::new(&mut (), ext_main2));
 
     match result1 {
         Ok(()) => panic!("Expected ExecutionError::Trap, but got Ok"),
@@ -588,17 +561,8 @@ fn test_invalid_jump_trap(config: Config) {
         }
     }
 
-    match result2 {
-        Ok(()) => panic!("Expected ExecutionError::Trap, but got Ok"),
-        Err(e) => {
-            if matches!(e, ExecutionError::Error(..)) {
-                panic!("Unexpected ExecutionError::Error: {:?}", e);
-            } else {
-                assert!(matches!(e, ExecutionError::Trap(..)), "Expected ExecutionError::Trap, but got {:?}", e);
-            }
-        }
-    }
 }
+
 
 fn test_blob_basic_test(config: Config) {
     let i = TestInstance::new(&config);
